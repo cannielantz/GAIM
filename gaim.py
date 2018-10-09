e92f5d9a6c31c16d0c5daeab5e91f727beae911e
Annie Lantz <carla162@student.liu.se>
2018-10-09 13:01:39 +0200

    New branch, to fix session error

--- gaim.py
+++ gaim.py
@@ -13,14 +13,121 @@ import midi_convert
 #sess = tf.Session()
 #print(sess.run(hello))
 
+###################### From midi to matrix ############################
+
 def get_songs(path):
     files = glob.glob('{}/*.mid*'.format(path)) #All midi-files in the folder
     songs = [] #An array to store the songs
     for f in tqdm(files):
-        song = np.array(midi_convert.midi2matrix(f)) #Convert each song to a note state matrix
-        if np.array(song).shape[0] > 50:
-            songs.append(song)
+        try:
+            song = np.array(midi_convert.midi2matrix(f)) #Convert each song to a note state matrix
+            if np.array(song).shape[0] > 50:
+                songs.append(song)
+        except Exception as e:
+            raise e
     return songs
 
 songs = get_songs('midi-songs')
 print("{} songs processed".format(len(songs)))
+
+######################################################################
+
+
+##################### Parameters #####################################
+
+lowestNote = midi_convert.min # Index of lowest note
+highestNote = midi_convert.max
+noteRange = highestNote - lowestNote
+
+timesteps = 15
+visible_size = 2 * noteRange * timesteps # Size of visible layer
+hidden_size = 50 # Size of hidden layer
+
+epochs = 200 # Number of training ephochs (through the entire dataset)
+batch_size = 100 # Number of training examples to send through the model at a time
+learning_rate = tf.constant(0.005, tf.float32)
+
+#####################################################################
+
+####################### Variables ###################################
+
+x = tf.placeholder(tf.float32, [None, visible_size], name = 'x') # Our data
+w = tf.Variable(tf.random_normal([visible_size, hidden_size], 0.01), name = 'w') # Weight matrix
+hidden_bias = tf.Variable(tf.zeros([1, hidden_size], tf.float32, name = 'hidden_bias'))
+visible_bias = tf.Variable(tf.zeros([1, visible_size], tf.float32, name = 'visible_bias'))
+
+#####################################################################
+
+########################### Functions ###############################
+
+def random_binary_vec(probs):
+    # Returns vector of random 0:s and 1:s from a probability vector
+    # This is for both h and x
+    return tf.floor(probs + tf.random_uniform(tf.shape(probs), 0, 1))
+
+
+def gibbs_chain(k):
+    def gibbs_step(count, k, xk):
+
+        hk = random_binary_vec(tf.sigmoid(tf.matmul(xk, w) + hidden_bias))
+        xk = random_binary_vec(tf.sigmoid(tf.matmul(hk, tf.transpose(w)) + visible_bias))
+
+        return count + 1, k, xk
+
+    counter = tf.constant(0)
+    [_,_, x_sample] = control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter, gibbs_step, [counter, tf.constant(k), x])
+
+    x_sample = tf.stop_gradient(x_sample)
+    return x_sample
+
+#########################################################################
+
+######################## Training update ################################
+
+x_sample = gibbs_chain(1)
+
+h = random_binary_vec(tf.sigmoid(tf.matmul(x, w) + hidden_bias))
+h_sample = random_binary_vec(tf.sigmoid(tf.matmul(x_sample, w) + hidden_bias))
+
+size_bt = tf.cast(tf.shape(x)[0], tf.float32)
+
+w_adder = tf.multiply(learning_rate/ size_bt, tf.subtract(tf.matmul(tf.transpose(x), h), tf.matmul(tf.transpose(x_sample), h_sample)))
+visible_bias_adder = tf.multiply(learning_rate / size_bt, tf.reduce_sum(tf.subtract(x, x_sample), 0, True))
+hidden_bias_adder = tf.multiply(learning_rate / size_bt, tf.reduce_sum(tf.subtract(h, h_sample), 0, True))
+
+update = [w.assign_add(w_adder), visible_bias.assign_add(visible_bias_adder), hidden_bias.assign_add(hidden_bias_adder)]
+
+##########################################################################
+
+########################### Train model ####################################
+
+with tf.Session() as sess:
+    # Train model
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    for epoch in tqdm(range(epochs)):
+        for song in songs:
+            song = np.array(song)
+            song = song[:int(np.floor(song.shape[0] // timesteps) * timesteps)]
+            song = np.reshape(song, [song.shape[0] // timesteps, song.shape[1] * timesteps])
+
+            for i in range(1, len(song), batch_size):
+                train_x = song[i:i + batch_size]
+                sess.run(update, feed_dict={x: train_x})
+
+###########################################################################
+
+########################## Make music ####################################
+
+sample = gibbs_chain(1).eval(session = sess, feed_dict={x: np.zeros((10, visible_size))})
+
+for i in range(sample.shape[0]):
+    if not any(sample[i, :]):
+        continue
+
+    S = np.reshape(sample[i, :], (timesteps, 2*noteRange))
+    midi_convert.matrix2midi(S, "generated/song_{}".format(i))
+
+
+
--- midi_convert.py
+++ midi_convert.py
@@ -70,4 +70,49 @@ def midi2matrix(midifile, squash=True, span=span):
     stateMatrix = np.asarray(stateMatrix).tolist()
     return stateMatrix
 
+def matrix2midi(statematrix, name='game_song', span=span):
+    statematrix = np.array(statematrix)
+
+    if not len(statematrix.shape) == 3:
+        statematrix = np.dstack((statematrix[:, :span], statematrix[:, span:]))
+    pattern = midi.pattern()
+    track = midi.Track()
+    pattern.append(track)
+
+    span = max - min
+    tickscale = 55
+
+    lastcmdtime = 0
+    prevstate = [[0, 0] for x in range(span)]
+
+    for time, state in enumerate(statematrix + [prevstate[:]]):
+        offNotes = []
+        onNotes = []
+
+        for i in range(span):
+            n = state[i]
+            p = prevstate[i]
+            if p[0] == 1:
+                if n[0] == 0:
+                    offNotes.append(i)
+                elif n[1] == 1:
+                    offNotes.append(i)
+                    onNotes.append(i)
+            elif n[0] == 1:
+                onNotes.append(i)
+        for note in offNotes:
+            track.append(midi.NoteOffEvent(tick = (time - lastcmdtime)*tickscale, pitch=note+min))
+            lastcmdtime = time
+        for note in onNotes:
+            track.append(midi.NoteOnEvent(tick = (time - lastcmdtime)*tickscale, velocity = 40, pitch = note + min))
+            lastcmdtime = time
+
+        prevstate = state
+
+    eot = midi.EndOfTrackEvent(tick=1)
+    track.append(eot)
+
+    midi.write_midifile("{}.midi".format(name), pattern)
+
+
 
